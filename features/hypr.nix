a4faaa9ae2a35f0814029c6c6088c40775a5684e
David Rumscheid <david.rumscheid@googlemail.com>
2024-02-21 16:16:52 +0100

    Change: Move Swaylock and Hyprpaper into hypr.nix

--- features/hypr.nix
+++ features/hypr.nix
@@ -8,12 +8,12 @@
     '';
   };
 
-  packages = with pkgs; [
+  home.packages = with pkgs; [
     swaylock-effects
     hyprpaper
   ];
 
   home.file.".config/hypr/hyprpaper.conf".source = ./hypr/hyprpaper.conf;
-  home.file.".config/hypr/wallpaper.jpg".source = ./hypr/wallpaper.jpg;
+  home.file.".config/hypr/hyprpaper/wallpaper.jpg".source = ./hypr/hyprpaper/wallpaper.jpg;
   home.file.".config/hypr/swaylock/swaylock-script.sh".source = ./hypr/swaylock/swaylock-script.sh;
 }
